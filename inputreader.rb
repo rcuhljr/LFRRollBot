65065e09a4cabb04499bc2bcf4b6c9e902513ee5
Robert Uhl <uhlrc@yahoo.com>
2010-11-15 15:08:36 -0500

    forgot new class

---
+++ inputreader.rb
@@ -0,0 +1,21 @@
+class InputReader
+  def initialize(canSend, outputBuffer, running)
+    @canSend = canSend
+    @outputBuffer = outputBuffer
+    @running = running
+    run()
+  end    
+
+  def run
+    while(@running[:state])
+      next if @canSend[:state]
+      @outputBuffer[0] = gets.chomp      
+      if(@outputBuffer[0] =~ /^quit/i)
+        @canSend[:state] = false
+        @running[:state] = false
+      elsif @outputBuffer[0].size > 0               
+        @canSend[:state] = true      
+      end
+    end
+  end  
+end
